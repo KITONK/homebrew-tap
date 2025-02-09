e3adc795a8b0808c73a94dcbcd691359c77c5381
KITO <kiyj585@gmail.com>
2025-02-09 13:50:41 +0200

    Update sha256

--- Formula/move2ssh.rb
+++ Formula/move2ssh.rb
@@ -2,7 +2,7 @@ class Move2ssh < Formula
     desc "A handy script for managing .ssh directories"
     homepage "https://github.com/KITONK/move2ssh"
     url "https://github.com/KITONK/move2ssh/archive/v1.0.0.tar.gz"
-    sha256 "09ee3b7b711cb3055268985e4644388c6465c5f7d4a1ebc62b06369d2d780d1f"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   
     depends_on "bash" => "4.0"
   
