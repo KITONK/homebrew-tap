397c40ee399fe4066843204e41a8bd75dc6ad0f9
KITO <kiyj585@gmail.com>
2025-04-25 16:54:35 +0300

    Update move2ssh to v1.2.0

--- Formula/move2ssh.rb
+++ Formula/move2ssh.rb
@@ -1,13 +1,35 @@
 class Move2ssh < Formula
     desc "A handy script for managing .ssh directories"
     homepage "https://github.com/KITONK/move2ssh"
-    url "https://github.com/KITONK/move2ssh/archive/v1.1.0.tar.gz"
-    sha256 "335f93832ef7f851c7db619d64f30eb1eb815c2209eaae5839c77bf34a79e5d9"
-  
-    depends_on "bash" => "4.0"
-  
+    url "https://github.com/KITONK/move2ssh/archive/v1.2.0.tar.gz"
+    sha256 "1e33af63548a4586c64f0310ae3994f14add12eed75c23cc27ce09b782270377"
+    license "MIT"
+    version "1.2.0"
+
+    head do
+      url "file://#{ENV['PWD']}/move2ssh"
+    end
+      
     def install
-      bin.install "move2ssh.sh"
+      bin.install "bin/move2ssh"
+
+      # Install completions
+      bash_completion.install "completions/bash/move2ssh"
+      zsh_completion.install "completions/zsh/_move2ssh"
+      fish_completion.install "completions/fish/move2ssh.fish"
+    
+      # PowerShell completion needs to be installed manually
+      prefix.install "completions/powershell/move2ssh.ps1"
+    end
+
+    def caveats
+      <<~EOS
+        PowerShell completion has been installed to:
+          #{prefix}/move2ssh.ps1
+        
+        Add the following to your PowerShell profile to enable completion:
+          . "#{prefix}/move2ssh.ps1"
+      EOS
     end
   
     test do
